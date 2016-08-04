734779ac1577244297d6468cf2b26a7aecc18f53
kabr <karen.broes@ordina.be>
2016-08-04 13:24:31 +0200

    #85 filter

---
+++ src/app/filter/filter.component.ts
@@ -0,0 +1,24 @@
+/*
+ * Angular 2 decorators and services
+ */
+import { Component } from '@angular/core';
+
+/*
+ * filter Component
+ */
+@Component({
+    selector: 'msd-filter',
+    templateUrl: `filter.html`
+})
+export class Msdfilter {
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+
+    }
+
+}
+
